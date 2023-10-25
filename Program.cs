d4d46421843c4a4ab8f11a40a63aabb3b20f3584
dinooo <145918377+dindooo@users.noreply.github.com>
2023-10-25 09:12:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezbaDruga
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisite niz znakova!!!");
+            string niz = Console.ReadLine();
+            
+                Console.WriteLine(niz.Replace(" ", "_"));
+
+            Console.ReadKey();
+        }
+    }
+}
